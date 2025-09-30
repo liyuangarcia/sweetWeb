a1250cd4dbd469f68c816ef7cdb7d4478ba31e9e
Liyuan Garcia Caballero <liyuangarcia@gmail.com>
2025-09-29 20:49:18 -0400

    Listado de Reservas.

--- src/pages/ReservasPage.vue
+++ src/pages/ReservasPage.vue
@@ -13,46 +13,51 @@
             />
 
             <div class="filter-section">
-              <div class="text-h6">Filtros de Búsqueda</div>
-              <q-separator class="q-my-md" />
-
               <div class="row q-col-gutter-md">
-                <div class="col-12 col-sm-6 col-md-3">
-                  <q-input filled v-model="filtros.cliente" label="Cliente" dense>
-                    <template v-slot:append>
-                      <q-icon name="search" />
-                    </template>
-                  </q-input>
-                </div>
-                <div class="col-12 col-sm-6 col-md-3">
-                  <q-input filled v-model="filtros.referencia" label="Referencia" dense>
-                    <template v-slot:append>
-                      <q-icon name="search" />
-                    </template>
-                  </q-input>
-                </div>
-                <div class="col-12 col-sm-6 col-md-3">
-                  <q-select
+                <div class="col-12 col-sm-12 col-md-12">
+                  <q-input
+                    @keyup.enter="getDatosGenerales()"
                     filled
-                    v-model="filtros.estado"
-                    :options="opcionesEstado"
-                    label="Estado"
+                    v-model="origenreservas_search"
+                    label="Aeropuertos"
                     dense
-                    emit-value
-                    map-options
-                  />
-                </div>
-                <div class="col-12 col-sm-6 col-md-3">
-                  <q-input filled v-model="filtros.fecha" label="Fecha" type="date" dense />
+                  >
+                    <template v-slot:append>
+                      <q-icon name="search" />
+                    </template>
+                  </q-input>
                 </div>
               </div>
 
               <div class="row justify-end q-mt-md">
-                <q-btn color="red-10" label="Buscar" icon="search" class="q-mr-md" />
-                <q-btn color="grey" label="Limpiar" outline />
+                <q-btn
+                  @click="getDatosGenerales()"
+                  color="red-10"
+                  label="Buscar"
+                  icon="search"
+                  class="q-mr-md"
+                />
+                <q-btn @click="clearDatosGenerales()" color="grey" label="Limpiar" outline />
               </div>
             </div>
-            <div class="reserva-table"><q-table :rows="rows" row-key="name" flat bordered /></div>
+            <div class="reserva-table">
+              <q-table
+                :rows="datosgenerales"
+                row-key="name"
+                :columns="reservascolums"
+                flat
+                bordered
+              >
+                <template v-slot:body-cell-actions="props">
+                  <q-td :props="props" class="text-center">
+                    <div class="q-gutter-xs">
+                      <q-btn icon="delete" color="red-10" size="sm" round dense title="Eliminar" />
+                      <q-btn icon="edit" color="grey" size="sm" round dense title="Editar" />
+                    </div>
+                  </q-td>
+                </template>
+              </q-table>
+            </div>
           </div>
         </q-page>
       </q-page-container>
@@ -62,122 +67,99 @@
 
 <script setup>
 import { ref } from 'vue'
+import { api } from 'boot/axios'
+import { onMounted } from 'vue'
 
-const rows = [
-  {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%',
-  },
-  {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: '8%',
-    iron: '1%',
-  },
-  {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: '6%',
-    iron: '7%',
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: '3%',
-    iron: '8%',
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: '7%',
-    iron: '16%',
-  },
-  {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: '0%',
-    iron: '0%',
-  },
-  {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: '0%',
-    iron: '2%',
-  },
-  {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: '0%',
-    iron: '45%',
-  },
-  {
-    name: 'Donut',
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: '2%',
-    iron: '22%',
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: '12%',
-    iron: '6%',
-  },
-]
+const origenreservas_search = ref('')
+const datosgenerales = ref([])
 
-const filtros = ref({
-  cliente: '',
-  referencia: '',
-  estado: null,
-  fecha: '',
+const reservascolums = ref([
+  {
+    name: 'actions',
+    label: 'Acciones',
+    field: 'actions',
+    align: 'center',
+  },
+  {
+    name: 'RFSOLICITUD',
+    align: 'left',
+    label: 'Fecha solicitud',
+    field: 'RFSOLICITUD',
+    sortable: true,
+  },
+  {
+    name: 'RORIGENDELARESERVA',
+    align: 'left',
+    label: 'Origen',
+    field: 'RORIGENDELARESERVAtext',
+    sortable: true,
+  },
+  {
+    name: 'RCORIGENDELARESERVA',
+    align: 'left',
+    label: 'Nº. Solicitud',
+    field: 'RCORIGENDELARESERVA',
+    sortable: true,
+  },
+  {
+    name: 'RCARPETA',
+    align: 'left',
+    label: 'Carpeta',
+    field: 'RCARPETA',
+    sortable: true,
+  },
+  {
+    name: 'RNOMBRE',
+    align: 'left',
+    label: 'Nombre del grupo',
+    field: 'RNOMBRE',
+    sortable: true,
+  },
+  {
+    name: 'RISERVICIO',
+    align: 'left',
+    label: 'Inicio de los Servicios',
+    field: 'RISERVICIO',
+    sortable: true,
+  },
+  {
+    name: 'RFSERVICIO',
+    align: 'left',
+    label: 'Fin de los Servicios',
+    field: 'RFSERVICIO',
+    sortable: true,
+  },
+  {
+    name: 'RDESTINOtext',
+    align: 'left',
+    label: 'Polo inicial',
+    field: 'RDESTINOtext',
+    sortable: true,
+  },
+])
+
+onMounted(() => {
+  getDatosGenerales()
 })
 
-const opcionesEstado = [
-  { label: 'Confirmada', value: 'Confirmada' },
-  { label: 'Pendiente', value: 'Pendiente' },
-  { label: 'Cancelada', value: 'Cancelada' },
-]
+const getDatosGenerales = async () => {
+  api
+    .get(`/datosgenerales/?search=${origenreservas_search.value}`)
+    .then((response) => {
+      datosgenerales.value = response.data
+      //this.totalRows = this.operators.length
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+const clearDatosGenerales = () => {
+  if (origenreservas_search.value) {
+    origenreservas_search.value = ''
+    getDatosGenerales()
+  }
+}
 </script>
 <style>
 .page-container {
@@ -196,18 +178,7 @@ const opcionesEstado = [
   border-radius: 8px;
   overflow: hidden;
 }
-.status-badge {
-  border-radius: 12px;
-  padding: 4px 12px;
-  font-size: 0.8em;
-  font-weight: 500;
-}
 .btn-agregar {
   margin-bottom: 20px;
-}
-.table-actions {
-  display: flex;
-  gap: 8px;
-  justify-content: center;
 }
 </style>
